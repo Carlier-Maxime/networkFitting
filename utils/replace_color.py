188fbb6b9b76c0cf43bb4e9424b6937c7dc79ede
maxime <maxime_carlier@ens.univ-artois.fr>
2023-04-13 10:05:17 +0200

    add replace_color

---
+++ utils/replace_color.py
@@ -0,0 +1,41 @@
+import click
+import torch
+from PIL import Image
+from torchvision.transforms import transforms
+
+def replaceColor(imgs,imgs_new_color,color,epsilon):
+    assert imgs.shape == imgs_new_color.shape
+    assert color.shape == torch.Size([3])
+    imgs = imgs.permute(0,2,3,1)
+    cond = (imgs>=(color-epsilon)) & (imgs<=(color+epsilon))
+    cond = cond[:,:,:,0] & cond[:,:,:,1] & cond[:,:,:,2]
+    imgs[cond]=imgs_new_color.permute(0,2,3,1)[cond]
+    imgs = imgs.permute(0,3,1,2)
+    return imgs
+
+def loadImg(path):
+    img = Image.open(path).convert('RGB')
+    return transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])(img)
+
+
+@click.command()
+@click.option('--img1','img1_path', type=str)
+@click.option('--img2','img2_path', type=str)
+@click.option('--device','device_name',default='cuda', type=torch.device)
+@click.option('--epsilon', type=float)
+@click.option('--color', help='a color list, value of composante in float range (-1)-(1)')
+@click.option('--outdir', default='out')
+def main(img1_path, img2_path, device_name, epsilon, color, outdir):
+    device = torch.device(device_name)
+    img1 = loadImg(img1_path).to(device)[None,:,:,:]
+    img2 = loadImg(img2_path).to(device)[None,:,:,:]
+    color = color[1:-1].split(',')
+    for i in range(len(color)): color[i]=float(color[i])
+    color = torch.tensor(color).to(device)
+    imgR = replaceColor(img1, img2, color, epsilon)
+    imgR = (imgR + 1) * (255/2)
+    imgR = imgR.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
+    Image.fromarray(imgR, 'RGB').save(f'{outdir}/replaced.png')
+
+if __name__ == '__main__':
+    main()
