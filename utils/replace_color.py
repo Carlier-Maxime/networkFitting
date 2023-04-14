29f94780aea8055bbde615b1434c27a7648645f3
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-04-14 14:34:14 +0200

    add pasteColor

--- utils/replace_color.py
+++ utils/replace_color.py
@@ -4,13 +4,25 @@ from PIL import Image
 from torchvision.transforms import transforms
 
 def replaceColor(imgs,imgs_new_color,color,epsilon):
-    assert imgs.shape == imgs_new_color.shape
+    assert imgs.shape == imgs_new_color.shape, f'Shape {imgs.shape} not equal {imgs_new_color.shape}'
     assert color.shape == torch.Size([3])
     imgs = imgs.permute(0,2,3,1)
     cond = (imgs>=(color-epsilon)) & (imgs<=(color+epsilon))
     cond = cond[:,:,:,0] & cond[:,:,:,1] & cond[:,:,:,2]
     imgs[cond]=imgs_new_color.permute(0,2,3,1)[cond]
     imgs = imgs.permute(0,3,1,2)
+    return imgs
+
+def pasteColor(imgs_color, imgs, color, epsilon):
+    assert imgs_color.shape == imgs.shape, f'Shape {imgs_color.shape} not equal {imgs.shape}'
+    assert color.shape == torch.Size([3])
+    imgs_color = imgs_color.permute(0,2,3,1)
+    cond = (imgs_color>=(color-epsilon)) & (imgs_color<=(color+epsilon))
+    cond = cond[:,:,:,0] & cond[:,:,:,1] & cond[:,:,:,2]
+    imgs = imgs.permute(0,2,3,1)
+    imgs[cond]=imgs_color[cond]
+    imgs = imgs.permute(0,3,1,2)
+    imgs_color = imgs_color.permute(0,3,1,2)
     return imgs
 
 def loadImg(path):
@@ -22,7 +34,7 @@ def loadImg(path):
 @click.option('--img1','img1_path', type=str)
 @click.option('--img2','img2_path', type=str)
 @click.option('--device','device_name',default='cuda', type=torch.device)
-@click.option('--epsilon', type=float)
+@click.option('--epsilon')
 @click.option('--color', help='a color list, value of composante in float range (-1)-(1)')
 @click.option('--outdir', default='out')
 def main(img1_path, img2_path, device_name, epsilon, color, outdir):
@@ -32,7 +44,12 @@ def main(img1_path, img2_path, device_name, epsilon, color, outdir):
     color = color[1:-1].split(',')
     for i in range(len(color)): color[i]=float(color[i])
     color = torch.tensor(color).to(device)
-    imgR = replaceColor(img1, img2, color, epsilon)
+    try: epsilon=float(epsilon)
+    except:
+        epsilon=epsilon[1:-1].split(',')
+        for i in range(len(epsilon)): epsilon[i]=float(epsilon[i])
+        epsilon = torch.tensor(epsilon).to(device)
+    imgR = pasteColor(img1, img2, color, epsilon)
     imgR = (imgR + 1) * (255/2)
     imgR = imgR.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
     Image.fromarray(imgR, 'RGB').save(f'{outdir}/replaced.png')
