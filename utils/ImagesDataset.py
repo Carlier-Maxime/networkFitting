7240480c02a0de410061655942064d78c7260d72
maxime_carlier <28m06c02@gmail.com>
2024-02-15 09:49:18 +0100

    reformat file

--- utils/ImagesDataset.py
+++ utils/ImagesDataset.py
@@ -10,11 +10,11 @@ from replace_color import rgb2hsv
 
 class ImagesDataset(Dataset):
 
-    def __init__(self, source_root, source_transform=None, dezired_size:int=-1, video_ips:int=60, verbose_make:bool=True, img_mode:str='auto'):
+    def __init__(self, source_root, source_transform=None, dezired_size: int = -1, video_ips: int = 60, verbose_make: bool = True, img_mode: str = 'auto'):
         self.source_paths = sorted(make_dataset(source_root, video_ips, verbose_make))
         self.source_transform = source_transform
-        self.dezired_size=dezired_size
-        self.img_mode=img_mode
+        self.dezired_size = dezired_size
+        self.img_mode = img_mode
 
     def __len__(self):
         return len(self.source_paths)
@@ -22,14 +22,14 @@ class ImagesDataset(Dataset):
     def __getitem__(self, index):
         fname, from_path = self.source_paths[index]
         from_im = Image.open(from_path)
-        if self.img_mode!='auto': from_im =from_im.convert(self.img_mode)
+        if self.img_mode != 'auto': from_im = from_im.convert(self.img_mode)
         w, h = from_im.size
         s = min(w, h)
         from_im = from_im.crop(((w - s) // 2, (h - s) // 2, (w + s) // 2, (h + s) // 2))
-        if self.dezired_size<=0: self.dezired_size=s
+        if self.dezired_size <= 0: self.dezired_size = s
         from_im = from_im.resize((self.dezired_size, self.dezired_size), Image.LANCZOS)
         tab_normalize = [0.5, 0.5, 0.5]
-        if from_im.mode=="RGBA": tab_normalize += [0.5]
+        if from_im.mode == "RGBA": tab_normalize += [0.5]
         return fname, transforms.Compose([transforms.ToTensor(), transforms.Normalize(tab_normalize, tab_normalize)])(from_im)
 
 
